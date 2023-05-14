271653ea3e635a3a5882d464f315cb78ee905cdb
khyatimirani <khyatimirani1995@gmail.com>
2023-05-14 17:04:27 +0530

    Updating the logic to get aggregated valuse

--- Classes/ClickMonitorHelper.m
+++ Classes/ClickMonitorHelper.m
@@ -11,36 +11,36 @@
 @implementation ClickMonitorHelper : NSObject
 NSMutableArray *clickTimeStamps = nil;
 -(BOOL)addEventToSystem:(NSString*)click :(NSString*)clickTimeStamp {
-    NSArray *array = [[NSArray alloc] initWithObjects:clickTimeStamp,nil];
     if (clickTimeStamps == nil)  {
         clickTimeStamps = [[NSMutableArray alloc]  initWithObjects:clickTimeStamp,nil];
     } else {
-        [clickTimeStamps addObject:array];
+        [clickTimeStamps addObject:clickTimeStamp];
     }
     return true;
 }
 -(float)getAggregatedAnswer:(int)timeStamp {
-    int *counter = 0;
-    NSDate *currentDate = [NSDate date];
+    int counter = 0;
     for (id object in clickTimeStamps) {
-    NSDate *date1 = [self convertStringToDate:object];
-        NSTimeInterval secondsBetween = [currentDate timeIntervalSinceDate:date1];
-        if (secondsBetween <= timeStamp) {
+        if ([self checkIfGivenIntervalMatches:object :timeStamp]) {
             counter = counter + 1;
         }
     }
-    float a = [[NSNumber numberWithInt: *counter] floatValue];
+    float a = [[NSNumber numberWithInt: counter] floatValue];
     return a;
 }
 
--(NSDate *)convertStringToDate:(NSString*)timeString {
-    NSString *dateStr = timeString;
-
+-(BOOL)checkIfGivenIntervalMatches:(NSString*)timeString :(int)forSeconds {
+    NSDate *currentDate = [NSDate date];
     // Convert string to date object
     NSDateFormatter *dateFormat = [[NSDateFormatter alloc] init];
-    [dateFormat setDateFormat:@"HH:mm:ss"];
-    NSDate *date = [dateFormat dateFromString:dateStr];
-    return date;
+    [dateFormat setDateFormat:@"yyyy-MM-dd'T'HH:mm:ss"];
+    NSDate *date = [dateFormat dateFromString:timeString];
+    NSTimeInterval secondsBetween = [currentDate timeIntervalSinceDate:date];
+    if (secondsBetween <= forSeconds) {
+        return true;
+    } else {
+        return  false;
+    }
 }
 
 @end
