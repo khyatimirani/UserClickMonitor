4deff0d5d1a8ec7e9805ea65435d82c46bf5b2af
khyatimirani <khyatimirani1995@gmail.com>
2023-05-25 10:26:59 +0530

    Added swift file to communicate to C++ wrapper

---
+++ Classes/ClickMonitor.swift
@@ -0,0 +1,29 @@
+//
+//  ClickMonitor.swift
+//  UserClickMonitor
+//
+//  Created by Khyati Mirani on 25/05/23.
+//
+
+import Foundation
+
+public class ClickMonitor: NSObject {
+    let cppWrapper = ClickMonitorCPPWrapper()
+    
+   public func addEventToSystem(map: [String: String]) -> Bool {
+        let timeStamp = getCurrentTime()
+        return cppWrapper.addEvent(toSystem: map.first?.key ?? "key1", timeStamp)
+    }
+    
+    private func getCurrentTime() -> String {
+        let mytime = Date()
+        let format = DateFormatter()
+        format.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+        return  format.string(from: mytime)
+    }
+    
+    public func getAggregatedAnswer(key: String) -> [Float] {
+        let clickCount = cppWrapper.getAggregatedAnswer(key)
+        return clickCount as! [Float]
+    }
+}
---
+++ podspec
@@ -0,0 +1,8 @@
+
+ -> UserClickMonitor -> UserClickMonitor
+    - ERROR | spec: The specification defined in `UserClickMonitor` could not be loaded.
+
+Is a directory @ io_fread - UserClickMonitor
+
+[!] UserClickMonitor did not pass validation, due to 1 error.
+You can use the `--no-clean` option to inspect any issue.
